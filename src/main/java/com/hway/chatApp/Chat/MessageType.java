4e74015f7c041f5c0ebb6d55ee0b367e428017ac
Dikshyanta Giri <giridikshyanta@gmail.com>
2024-01-02 13:03:49 +0545

    Chat Application
    A simple Chat Application using Spring Boot and WebSocket with minimal use of HTML/CSS/JS. In this application we can connect with other user and chat with them in real time.

---
+++ src/main/java/com/hway/chatApp/Chat/MessageType.java
@@ -0,0 +1,9 @@
+package com.hway.chatApp.Chat;
+
+public enum MessageType {
+	
+	CHAT,
+	JOIN,
+	LEAVE
+
+}
---
+++ src/main/java/com/hway/chatApp/config/WebSocketEventListner.java
@@ -0,0 +1,38 @@
+package com.hway.chatApp.config;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.hway.chatApp.Chat.ChatMessage;
+import com.hway.chatApp.Chat.MessageType;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+
+public class WebSocketEventListner {
+	
+	private final SimpMessageSendingOperations messageTemplate;
+
+	@EventListener
+	public void handleWebSocketDisconnetListener(SessionDisconnectEvent event) {
+		
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+		String username = (String) headerAccessor.getSessionAttributes().get("username");
+		if(username != null) {
+			log.info("User disconnected: {}", username);
+			var chatMessage = ChatMessage.builder()
+					.type(MessageType.LEAVE)
+					.sender(username)
+					.build();
+			messageTemplate.convertAndSend("/topic/public",chatMessage);
+		}
+		
+	}
+}
